2239f21c9a5372557a90c0b448fd6da9c92b3cae
Gabro057 <gabro057@seznam.cz>
2022-07-06 22:20:08 +0200

    Setting react-redux and redux-thunk

--- src/reducers/gamesReducers.js
+++ src/reducers/gamesReducers.js
@@ -7,11 +7,20 @@ const initState = {
 const gamesReducers = (state=initState, action) => {
   switch(action.type){
     case "FETCH_GAMES":
-      return {...state}
+      return {...state} //return {...state, ..action.payload}
     default:
       return {...state}
   }
-
 }
+
+//ACTION
+/*const fetchGames = userData => {
+  return {
+    type: "FETCH_GAMES",
+    payload: userData
+  }
+}*/
+
+
 
 export default gamesReducers
